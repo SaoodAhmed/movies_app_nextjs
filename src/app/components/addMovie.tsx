55e3e591a6d576d7bd1a59bfa6385682c3974cbf
Saood Ahmed <107537712+SaoodAhmed@users.noreply.github.com>
2024-02-15 14:05:26 +0500

    movie app nextjs

--- src/app/components/addMovie.tsx
+++ src/app/components/addMovie.tsx
@@ -38,12 +38,12 @@ const AddMovie: React.FC<{ addMovie: any}> = ({ addMovie }) => {
 
   return (
     <div className='mt-6'>
-        <form onSubmit={(e)=>handleAdd(e)} className='flex justify-center mx-auto flex-col w-4/12 gap-y-4 ' >
-            <Input className=' outline-none text-slate-600 focus:purple-800  focus:border-blue-600' placeholder='Enter image url' value={data.imgSrc} name="imgSrc" onChange={handleOnChange}/>
+        <form onSubmit={(e)=>handleAdd(e)} className='flex justify-center items-center flex-col gap-y-4 ' >
+            <Input className=' shrink-0 outline-none text-slate-600 focus:purple-800  focus:border-blue-600' placeholder='Enter image url' value={data.imgSrc} name="imgSrc" onChange={handleOnChange}/>
             <Input className='outline-none text-slate-600 focus:purple-800 focus:border-blue-600' placeholder='Enter movie name' value={data.name} name='name' onChange={handleOnChange}/>
             <Input className=' outline-none text-slate-600 focus:purple-800 focus:border-blue-600' placeholder='Enter movie category' value={data.category} name='category' onChange={handleOnChange}/>
             <Input className=' outline-none text-slate-600 focus:purple-800 focus:border-blue-600' placeholder='Enter movie url' value={data.link} name='link' onChange={handleOnChange}/>
-            <button type="submit" className="bg-gradient-to-t text-slate-100 w-48 text-lg py-2 mx-auto rounded-md from-purple-800 via-indigo-700 to-blue-500 font-medium">
+            <button type="submit" className="bg-gradient-to-t text-slate-100 w-36 text-lg py-2 rounded-md from-purple-800 via-indigo-700 to-blue-500 font-medium">
             Add Movie
             </button>   
         </form>
